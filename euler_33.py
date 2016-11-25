50001cfe7a111890fe0d84ac4b3dd34a2b8423e7
Ajay Mandyam Rangarajan <ajay.rangarajan@rwth-aachen.de>
2016-11-25 18:48:11 +0100

    euler problems33-35

---
+++ euler_33.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+prod = 1.0
+for a in range(1, 10):
+	for b in range(1, 10):
+		for c in range(1, 10):
+			if(a!=b and b!=c):
+				lhs = 9*a*c
+				rhs = b*(10*a-c)
+				if(lhs==rhs):
+					prod = prod * float(a)/float(c)
+print prod*100
+# for a in range(1, 10):
+# 	for b in range(1, 10):
+# 		for c in range(1, 10):
+# 			if(a!=b and b!=c):
+# 				lhs = 9*b*c
+# 				rhs = a*(10*c-b)
+# 				if(lhs==rhs):
+# 					print "Eq2 ",10*a+b,"/",10*c+a
+# 					
+"""
+https://projecteuler.net/problem=33
+"""
